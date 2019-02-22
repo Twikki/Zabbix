14ea47c24a4a8f2debebd4029bc2789af99201b2
Daniel Jean Schmidt <46316044+Twikki@users.noreply.github.com>
2019-02-23 00:32:54 +0100

    Add files via upload

--- Zabbix_Automated_Install.ps1
+++ Zabbix_Automated_Install.ps1
@@ -1,20 +1,22 @@
 # Install Zabbix agent on Windows
 # Tested on Windows Server 2016, Virtual Machine
-# Version 1.0
+# Version 1.01
 # Created by Daniel Jean Schmidt
-# Last updated 13/02/2019
+# Last updated 23/02/2019
 # Installs Zabbix Agent 4.0.4
 
 
+#Gets the server host name
+$ServerHostname = "$ENV:COMPUTERNAME"
+
+
 # Asks the user for the IP address of their Zabbix server
-$ServerIP = Read-Host -Prompt 'What is your Zabbix server IP?'
+$ServerIP = Read-Host -Prompt 'What is your Zabbix server/proxy IP?'
 
-
-# Deletes Zabbix DIR if already exists
-Remove-Item C:\Zabbix -Force
 
 # Creates Zabbix DIR
 mkdir C:\Zabbix
+
 
 # Downloads version 4.0.4 from Zabbix.com
 wget "https://www.zabbix.com/downloads/4.0.4/zabbix_agents-4.0.4-win-amd64.zip" -outfile C:\Zabbix\Zabbix-4.0.4.zip
@@ -38,8 +40,11 @@ Move-Item C:\Zabbix\bin\zabbix_agentd.exe -Destination C:\Zabbix
 # Sorts files in C:\Zabbix
 Move-Item C:\Zabbix\conf\zabbix_agentd.win.conf -Destination C:\Zabbix
 
-# Replaces 127.0.0.1 with your Zabbix server IP
+# Replaces 127.0.0.1 with your Zabbix server IP in the config file
 (Get-Content -Path C:\Zabbix\zabbix_agentd.win.conf) | ForEach-Object {$_ -Replace '127.0.0.1', "$ServerIP"} | Set-Content -Path C:\Zabbix\zabbix_agentd.win.conf
+
+# Replaces hostname in the config file
+(Get-Content -Path C:\Zabbix\zabbix_agentd.win.conf) | ForEach-Object {$_ -Replace 'Windows host', "$ServerHostname"} | Set-Content -Path C:\Zabbix\zabbix_agentd.win.conf
 
 # Attempts to install the agent with the config in C:\Zabbix
 C:\Zabbix\zabbix_agentd.exe --config C:\Zabbix\zabbix_agentd.win.conf --install
