bfe6674a86577bbc043edfc407f8d696b35a8d93
Daniel Jean Schmidt <46316044+Twikki@users.noreply.github.com>
2019-02-28 09:27:00 +0100

    Add files via upload

--- Zabbix_Automated_Install.ps1
+++ Zabbix_Automated_Install.ps1
@@ -1,8 +1,8 @@
 # Install Zabbix agent on Windows
 # Tested on Windows Server 2016, Virtual Machine
-# Version 1.01
+# Version 1.02
 # Created by Daniel Jean Schmidt
-# Last updated 23/02/2019
+# Last updated 28/02/2019
 # Installs Zabbix Agent 4.0.4
 
 
@@ -15,11 +15,11 @@ $ServerIP = Read-Host -Prompt 'What is your Zabbix server/proxy IP?'
 
 
 # Creates Zabbix DIR
-mkdir C:\Zabbix
+mkdir c:\zabbix
 
 
 # Downloads version 4.0.4 from Zabbix.com
-wget "https://www.zabbix.com/downloads/4.0.4/zabbix_agents-4.0.4-win-amd64.zip" -outfile C:\Zabbix\Zabbix-4.0.4.zip
+wget "https://www.zabbix.com/downloads/4.0.4/zabbix_agents-4.0.4-win-amd64.zip" -outfile c:\zabbix\zabbix-4.0.4.zip
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
@@ -29,28 +29,28 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-# Unzipping file to C:\Zabbix
-Unzip "C:\Zabbix\Zabbix-4.0.4.zip" "C:\Zabbix"      
+# Unzipping file to c:\zabbix
+Unzip "c:\Zabbix\zabbix-4.0.4.zip" "c:\zabbix"      
 
 
-# Sorts files in C:\Zabbix
-Move-Item C:\Zabbix\bin\zabbix_agentd.exe -Destination C:\Zabbix
+# Sorts files in c:\zabbix
+Move-Item c:\zabbix\bin\zabbix_agentd.exe -Destination c:\zabbix
 
 
-# Sorts files in C:\Zabbix
-Move-Item C:\Zabbix\conf\zabbix_agentd.win.conf -Destination C:\Zabbix
+# Sorts files in c:\zabbix
+Move-Item c:\zabbix\conf\zabbix_agentd.win.conf -Destination c:\zabbix
 
 # Replaces 127.0.0.1 with your Zabbix server IP in the config file
-(Get-Content -Path C:\Zabbix\zabbix_agentd.win.conf) | ForEach-Object {$_ -Replace '127.0.0.1', "$ServerIP"} | Set-Content -Path C:\Zabbix\zabbix_agentd.win.conf
+(Get-Content -Path c:\zabbix\zabbix_agentd.win.conf) | ForEach-Object {$_ -Replace '127.0.0.1', "$ServerIP"} | Set-Content -Path c:\zabbix\zabbix_agentd.win.conf
 
 # Replaces hostname in the config file
-(Get-Content -Path C:\Zabbix\zabbix_agentd.win.conf) | ForEach-Object {$_ -Replace 'Windows host', "$ServerHostname"} | Set-Content -Path C:\Zabbix\zabbix_agentd.win.conf
+(Get-Content -Path c:\zabbix\zabbix_agentd.win.conf) | ForEach-Object {$_ -Replace 'Windows host', "$ServerHostname"} | Set-Content -Path c:\zabbix\zabbix_agentd.win.conf
 
-# Attempts to install the agent with the config in C:\Zabbix
-C:\Zabbix\zabbix_agentd.exe --config C:\Zabbix\zabbix_agentd.win.conf --install
+# Attempts to install the agent with the config in c:\zabbix
+c:\zabbix\zabbix_agentd.exe --config c:\zabbix\zabbix_agentd.win.conf --install
 
 # Attempts to start the agent
-C:\Zabbix\zabbix_agentd.exe --start
+c:\zabbix\zabbix_agentd.exe --start
 
 # Creates a firewall rule for the Zabbix server
-New-NetFirewallRule -DisplayName "Allow Zabbix communication" -Direction Inbound -Program "C:\Zabbix\Zabbix_agentd.exe" -RemoteAddress LocalSubnet -Action Allow
+New-NetFirewallRule -DisplayName "Allow Zabbix communication" -Direction Inbound -Program "c:\zabbix\zabbix_agentd.exe" -RemoteAddress LocalSubnet -Action Allow
--- Zabbix_Automated_Update_Existing_Agent.ps1
+++ Zabbix_Automated_Update_Existing_Agent.ps1
@@ -1,8 +1,8 @@
 # Update Zabbix agent on Windows
 # Tested on Windows Server 2016, Virtual Machine
-# Version 1.0
+# Version 1.01
 # Created by Daniel Jean Schmidt
-# Last updated 13/02/2019
+# Last updated 28/02/2019
 # Attempts to install the latest version of Zabbix
 
 
@@ -10,10 +10,10 @@
 ############################################################
 
 # Attempts to stop the Zabbix service on the Windows machine
-C:\Zabbix\zabbix_agentd.exe --stop
+c:\zabbix\zabbix_agentd.exe --stop
 
 # Attempts to uninstall the Zabbix agent on the Windows machine
-C:\Zabbix\zabbix_agentd.exe --uninstall
+c:\zabbix\zabbix_agentd.exe --uninstall
 
 ############################################################
 
@@ -22,19 +22,19 @@ C:\Zabbix\zabbix_agentd.exe --uninstall
 ############################################################
 
 # Creates Zabbix backup destination for Zabbix config file
-mkdir C:\Zabbix_Backup
+mkdir c:\zabbix_backup
 
 # Backs up the config file
-Copy-Item C:\Zabbix\*.conf -Destination C:\Zabbix_Backup
+Copy-Item c:\zabbix\*.conf -Destination c:\zabbix_backup
 
 # Deletes the old contents
-Remove-Item C:\Zabbix -Force -Recurse
+Remove-Item c:\zabbix -Force -Recurse
 
 # Creates Zabbix folder again
-mkdir C:\Zabbix
+mkdir c:\zabbix
 
-# Downloads version 4.0.4 from Zabbix.com to C:\Zabbix
-wget "https://www.zabbix.com/downloads/4.0.4/zabbix_agents-4.0.4-win-amd64.zip" -outfile C:\Zabbix\Zabbix-4.0.4.zip
+# Downloads version 4.0.4 from Zabbix.com to c:\zabbix
+wget "https://www.zabbix.com/downloads/4.0.4/zabbix_agents-4.0.4-win-amd64.zip" -outfile c:\zabbix\zabbix-4.0.4.zip
 
 # Imports ZIP
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -45,25 +45,25 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-# Unzipping file to C:\Zabbix
-Unzip "C:\Zabbix\Zabbix-4.0.4.zip" "C:\Zabbix"
+# Unzipping file to c:\zabbix
+Unzip "c:\zabbix\zabbix-4.0.4.zip" "c:\zabbix"
 
 
-# Sorts files in C:\Zabbix
-Move-Item C:\Zabbix\bin\zabbix_agentd.exe -Destination C:\Zabbix
+# Sorts files in c:\zabbix
+Move-Item c:\zabbix\bin\zabbix_agentd.exe -Destination c:\zabbix
 
 
-# Sorts files in C:\Zabbix
-Move-Item C:\Zabbix\conf\zabbix_agentd.win.conf -Destination C:\Zabbix
+# Sorts files in c:\zabbix
+Move-Item c:\zabbix\conf\zabbix_agentd.win.conf -Destination c:\zabbix
 
-# Copies the old config file back into the C:\Zabbix
-Copy-Item C:\Zabbix_Backup\*.conf -Destination C:\Zabbix -Force
+# Copies the old config file back into the c:\zabbix
+Copy-Item c:\zabbix_backup\*.conf -Destination c:\zabbix -Force
 
-# Cleans up C:\Zabbix_Backup folder
-Remove-Item C:\Zabbix_Backup -Force -Recurse
+# Cleans up c:\zabbix_Backup folder
+Remove-Item c:\zabbix_backup -Force -Recurse
 
-# Attempts to install the agent with the config in C:\Zabbix
-C:\Zabbix\zabbix_agentd.exe --config C:\Zabbix\zabbix_agentd.win.conf --install
+# Attempts to install the agent with the config in c:\zabbix
+c:\zabbix\zabbix_agentd.exe --config c:\zabbix\zabbix_agentd.win.conf --install
 
 # Attempts to start the agent
-C:\Zabbix\zabbix_agentd.exe --start
+c:\zabbix\zabbix_agentd.exe --start
