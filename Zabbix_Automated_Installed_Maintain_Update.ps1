222bd1eaf4dddc3b59cb8cd322b4ab2bb38c30bf
Twikki <46316044+Twikki@users.noreply.github.com>
2019-09-09 14:25:48 +0200

    Update Zabbix_Automated_Installed_Maintain_Update.ps1

--- Zabbix_Automated_Installed_Maintain_Update.ps1
+++ Zabbix_Automated_Installed_Maintain_Update.ps1
@@ -1,4 +1,9 @@
 # Short description of script
-# Installs the Zabbix agent if not installed, maintains the agent if installed, or updates the agent
-# if not up to date.
+# USED FOR ACTIVE DIRECTORY!
+# Installs the Zabbix agent if not installed, maintains the agent if installed, or updates the agent if not up to date.
 # This script will be using private Github repositories for maintain the files
+
+# Used for later
+#$computerliste = Get-ADComputer -Properties operatingsystem  -Filter {(operatingsystem -like "* Server *") -and (enabled -eq $true)
+
+
