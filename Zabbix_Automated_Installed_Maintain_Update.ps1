22c767fc512491582909376eeaabd48f3d8da667
Twikki <46316044+Twikki@users.noreply.github.com>
2019-09-09 14:21:29 +0200

    Create Zabbix_Automated_Installed_Maintain_Update.ps1

---
+++ Zabbix_Automated_Installed_Maintain_Update.ps1
@@ -0,0 +1,4 @@
+# Short description of script
+# Installs the Zabbix agent if not installed, maintains the agent if installed, or updates the agent
+# if not up to date.
+# This script will be using private Github repositories for maintain the files
