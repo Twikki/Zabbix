8d2a88535178dc08feae454840f6c9e3b45e4bb5
Twikki <46316044+Twikki@users.noreply.github.com>
2019-09-10 09:43:06 +0200

    Create Zabbix_Automated_Remote_install_Maintain_Update.ps1

---
+++ Zabbix_Automated_Remote_install_Maintain_Update.ps1
@@ -0,0 +1,5 @@
+# Short description of script
+# THIS IS USED FOR PUSHING ZABBIX AGENTS TO REMOTE IP ADDRESSES
+# Installs the Zabbix agent if not installed, maintains the agent if installed, or updates the agent if not up to date.
+# This script will be using private Github repositories for maintaining the files
+
