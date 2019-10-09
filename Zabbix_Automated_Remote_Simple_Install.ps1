a512e693a1dccce5de70a81a9d5deb1cfa524e73
Twikki <46316044+Twikki@users.noreply.github.com>
2019-10-09 12:56:46 +0200

    Update Zabbix_Automated_Remote_Simple_Install.ps1

--- Zabbix_Automated_Remote_Simple_Install.ps1
+++ Zabbix_Automated_Remote_Simple_Install.ps1
@@ -1,6 +1,6 @@
 # Short description of script
 # THIS IS USED FOR PUSHING ZABBIX AGENTS TO REMOTE IP ADDRESSES
-# Installs the Zabbix agent if not installed, maintains the agent if installed, or updates the agent if not up to date.
+# Installs the Zabbix agent if not installed
 # This script will be using private Github repositories for maintaining the files
 #
 # CSV file must be as following:
