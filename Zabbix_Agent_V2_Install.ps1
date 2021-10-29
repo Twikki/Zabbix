6f45578c445bee6e4f01120689c30619d3ea614b
Twikki <46316044+Twikki@users.noreply.github.com>
2021-10-29 15:39:19 +0200

    Update Zabbix_Agent_V2_Install.ps1

--- Zabbix_Agent_V2_Install.ps1
+++ Zabbix_Agent_V2_Install.ps1
@@ -5,14 +5,14 @@
 # Install Zabbix agent V2 on Windows
 # This script is designed for automatic install and maintenance via Endpoint manager
 # Tested on Windows 10
-# Version 1.00
+# Version 1.02
 # Created by Twikki
-# Last updated 21/07/2021
+# Last updated 29/10/2021
 
 
 # Download links for different versions 
 # 
-$version523ssl = "https://cdn.zabbix.com/zabbix/binaries/stable/5.4/5.4.3/zabbix_agent2-5.4.3-windows-amd64-openssl-static.zip"
+$versionssl = "https://cdn.zabbix.com/zabbix/binaries/stable/5.4/5.4.7/zabbix_agent2-5.4.7-windows-amd64-openssl-static.zip"
 
 #Gets the server host name
 $serverHostname =  Invoke-Command -ScriptBlock {hostname}
@@ -23,8 +23,8 @@ $ServerIP = Read-Host -Prompt 'What is your Zabbix server/proxy IP?'
 # Creates Zabbix DIR
 mkdir c:\zabbix
 
-# Downloads the version you want. Links are up. This script currently as standard downloads version 5.0.0 with SSL option
-Invoke-WebRequest "$version523ssl" -outfile c:\zabbix\zabbix.zip
+# Downloads the version you want. Links are up. This script currently as standard downloads version 5.4.7 with SSL option
+Invoke-WebRequest "$versionssl" -outfile c:\zabbix\zabbix.zip
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
@@ -50,4 +50,4 @@ C:\zabbix\bin\zabbix_agent2.exe --config c:\zabbix\conf\zabbix_agent2.conf --ins
 c:\zabbix\bin\zabbix_agent2.exe --start --config C:\zabbix\conf\zabbix_agent2.conf
 
 # Creates a firewall rule for the Zabbix server
-New-NetFirewallRule -DisplayName "Allow Zabbix communication" -Direction Inbound -Program "c:\zabbix\\binzabbix_agent2.exe" -RemoteAddress LocalSubnet -Action Allow
+New-NetFirewallRule -DisplayName "Allow Zabbix communication" -Direction Inbound -Program "c:\zabbix\bin\zabbix_agent2.exe" -RemoteAddress LocalSubnet -Action Allow
